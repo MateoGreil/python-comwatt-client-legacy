ae87dd0fe00b66c2ff09818f97d344730881d481
Mateo Greil <mat@greil.fr>
2025-01-27 16:14:56 +0100

    🔖 Release v0.1.1

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
 setup(
     name='comwatt-client-legacy',
-    version='0.1',
+    version='0.1.2',
     author='Matéo Greil',
     author_email='contact@greil.fr',
-    description='Python Client for Comwatt API',
+    description='Python Client for Comwatt API (legacy: go.energy.comwatt)',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/MateoGreil/python-comwatt-client-legacy",
     packages=find_packages(),
     install_requires=[
         'requests',
